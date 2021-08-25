07135cc1dd01e55c799bc2657b0b102bd7c9c90d
SheeranJL <lewisjsheeran@protonmail.com>
2021-08-25 13:40:38 +1000

    Final edits and changes
    Added

--- js/script.js
+++ js/script.js
@@ -50,10 +50,23 @@ function getRandomQuote () {
   return quotes[randomNumber];
 }
 
+//*** Here i've created a function which creates a random HEX value and assigns that value to the color variable.
+//*** The color variable is concatinated with both the randomHex variable string (1-F) which is futher manipulated by...
+//*** using the Math.random function within a for loop to randomly choose those hex values.
+//*** the function then changes the body background color. (I added another event listened line at the bottom of the code to call this function each time the button is clicked.)
+function backgroundColor() {
+  let color = '#';
+  let randomHex = '123456789ABCDEF';
+  for (let i = 0; i < 6; i ++) {
+    color += randomHex[Math.floor(Math.random() * 15)];
+    document.body.style.background = color;
+  }
+  return color;
+}
 
 /***
  * `printQuote` function - This function I created calls the getRandomQuote function above and then assigns the quote to a variable named quoteObject.
-    Using template literals (because I find them way easier to work with) I started constructing my HTML and inserted the quote using the quoteObject variable
+    Using template literals (because I find them way easier to work with) I started constructing my HTML and inserted the quote using the quoteObject variable...
     and specifically targeted the properties that were relevant for each section of the string.
     Furthermore, I created two IF statements to run in the event there was also a citation or year property associated with the object.
     I was thinking of using (xx ||(or)&&(and) xx) statement, however I wasn't sure if this would work as well so I kept things simple.
@@ -73,11 +86,20 @@ function printQuote() {
     document.getElementById('quote-box').innerHTML = html;
 }
 
-printQuote();
+
 
 /***
  * click event listener for the print quote button
  * DO NOT CHANGE THE CODE BELOW!!
 ***/
 
+
+function refreshInterval(quote, background){
+  setInterval(quote, 7000)
+  setInterval(background, 7000)
+};
+
+refreshInterval(printQuote, backgroundColor);
+
 document.getElementById('load-quote').addEventListener("click", printQuote, false);
+document.getElementById('load-quote').addEventListener("click", backgroundColor, false);
